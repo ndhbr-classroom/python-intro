da62b90bed589dcf2098e396ac1ac494b1bdffc4
Andreas Huber <info@ndhbr.de>
2022-04-05 18:08:55 +0200

    Fixed indentation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from name import print_name
 
 if __name__ == "__main__":
-    name = input("Enter your name: ")
-    print_name(name)
+	name = input("Enter your name: ")
+	print_name(name)
--- name.py
+++ name.py
@@ -1,4 +1,4 @@
 def print_name(name):
-    # TODO: Gib den übergebenen Namen in diesem Format aus:
-    # Hallo, mein Name ist NAME
-    return
+	# TODO: Gib den übergebenen Namen in diesem Format aus:
+	# Hallo, mein Name ist NAME
+	return
